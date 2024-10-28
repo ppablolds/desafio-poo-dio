d6321fb2f777ff505b1e520d9a94bb03ffe3ceed
Pablo Silva <pablosilvaaluno@outlook.com>
2024-10-28 13:32:44 -0300

    Criando classes Bootcamp e Dev

---
+++ src/br/com/dio/desafios/dominio/Bootcamp.java
@@ -0,0 +1,92 @@
+package br.com.dio.desafios.dominio;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Bootcamp {
+    private String nome;
+    private String descricacao;
+    private final LocalDate dataInicial = LocalDate.now();
+    private final LocalDate dataFinal = dataInicial.plusDays(45);
+    private Set<Dev> devsInscritos = new HashSet<>();
+    private Set<Conteudo> conteudos = new LinkedHashSet<>();
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricacao() {
+        return descricacao;
+    }
+
+    public void setDescricacao(String descricacao) {
+        this.descricacao = descricacao;
+    }
+
+    public Set<Dev> getDevsInscritos() {
+        return devsInscritos;
+    }
+
+    public void setDevsInscritos(Set<Dev> devsInscritos) {
+        this.devsInscritos = devsInscritos;
+    }
+
+    public Set<Conteudo> getConteudos() {
+        return conteudos;
+    }
+
+    public void setConteudos(Set<Conteudo> conteudos) {
+        this.conteudos = conteudos;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 23 * hash + Objects.hashCode(this.nome);
+        hash = 23 * hash + Objects.hashCode(this.descricacao);
+        hash = 23 * hash + Objects.hashCode(this.dataInicial);
+        hash = 23 * hash + Objects.hashCode(this.dataFinal);
+        hash = 23 * hash + Objects.hashCode(this.devsInscritos);
+        hash = 23 * hash + Objects.hashCode(this.conteudos);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Bootcamp other = (Bootcamp) obj;
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (!Objects.equals(this.descricacao, other.descricacao)) {
+            return false;
+        }
+        if (!Objects.equals(this.dataInicial, other.dataInicial)) {
+            return false;
+        }
+        if (!Objects.equals(this.dataFinal, other.dataFinal)) {
+            return false;
+        }
+        if (!Objects.equals(this.devsInscritos, other.devsInscritos)) {
+            return false;
+        }
+        return Objects.equals(this.conteudos, other.conteudos);
+    }
+
+    
+}
